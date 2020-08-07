a7eaa91304ca6c8bd3703b390870105a9d40c19f
tiavina-mika <ralainirinatiavina@gmail.com>
2020-08-07 15:48:18 +0300

    fix packages version

--- demo-design/src/App.tsx
+++ demo-design/src/App.tsx
@@ -1,9 +1,33 @@
 import React, { FC } from 'react';
+import { ThemeProvider } from 'react-jss';
+
 import logo from './logo.svg';
 import './App.css';
 
+const theme = {
+  color: {
+      active: '#99CA3D', //green
+      secondary: '#0E203A', //blue dark
+      primary: '#1D174F', // blue dark
+      grey: '#EFF0F3', //grey light
+      secondaryHover: '#7ea530', //green
+      secondaryBlue: '#05396B', //blue lighter
+      secondaryGrey: 'rgba(14,32,58,0.6)', //grey with opacity
+      altGrey: '#DDDDDD', //grey used on simulator
+      white: '#FFFFFF',
+  },
+  font: {
+      poppins: 'Poppins, sans-serif',
+      montserrat: 'Montserrat, sans-serif',
+      proximaNova: 'Proxima Nova',
+      bold: 'bold',
+  },
+  spacing: (size: number): number => size * 6,
+};
+
 const App: FC = () => {
     return (
+      <ThemeProvider theme={theme}>
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
@@ -14,7 +38,8 @@ const App: FC = () => {
                     Learn React
                 </a>
             </header>
-        </div>
+        </div>        
+      </ThemeProvider>
     );
 };
 
